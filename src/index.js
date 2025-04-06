94a6ae4de729a64982be4ec1c3e2c414c1a2b9cc
ちとせあめ <chitoseame.jp@gmail.com>
2025-04-06 15:48:48 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+const {Client,Events,GatewayIntentBits}=require("discord.js");
+const checkBlackWords=require("./utils/filter");
+const token=process.env.TOKEN;
 
+const client=new Client({intents:[
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.MessageContent,
+	GatewayIntentBits.GuildMessages
+]});
+
+client.once(Events.ClientReady,c=>{
+	console.log(`準備OKです! ${c.user.tag}がログインします。`);
+});
+
+client.on(Events.MessageCreate,async m=>{
+	if(m.author.bot)return;
+	console.log(`受信しました${m}`);
+	if(!m.system&&!m.mentions.everyone&&m.mentions.users.has("1345636416566333533")){
+		let content =m.content.replace(/(?:<@[\d]+?> )*?/,"");
+		console.log(content);
+		if(checkBlackWords(content))returnm.reply("禁止ワードが検出されました");
+		m.reply(content);
+	}
+});
+
+client.login(token);
+
+//Koyebの仕様でhttpサーバーを立てないといけない
+const http=require("http");
+
+const server=http.createServer((req,res)=>{
+  res.end();
+});
+
+const port=process.env.PORT||8080;
+server.listen(port,()=>console.log(`Server is running on PORT ${port}`));
