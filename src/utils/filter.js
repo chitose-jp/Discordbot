a62ead837a11d5feeec3f9c3b5806dfab48fd46c
ちとせあめ <chitoseame.jp@gmail.com>
2025-04-06 15:49:45 +0900

    Update filter.js

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,1 +1,11 @@
+const bannedWords=JSON.parse(require("fs").readFileSync("bannedWords.json"));
 
+const reg=new RegExp(`(?:${bannedWords.join("|")})`);
+
+function checkBannedWords(str){
+  const match=String(str).match(reg);
+  if(match)return true;
+  else return false;
+}
+
+module.exports=checkBannedWords;
