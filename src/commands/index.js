da3a0666ac5e36b2189ebf9e3698ac17cf019a80
ちとせあめ <chitoseame.jp@gmail.com>
2025-04-06 22:13:02 +0900

    Update index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,16 +1,18 @@
 async function handleCommand(interaction){
-  console.log(interaction);
   if(!interaction.isChatInputCommand())return;
 
   if(interaction.commandName==="ping") {
     await interaction.reply({content:"Pong!",ephemeral:true});
   }
-  if(interaction.commandName==="info server") {
-    const servsr=interaction.guild;
-    console.log(server);
-    await interaction.reply({content:`サーバー名:${server.name}\n
-    メンバー数:${server.memberCount}\n
-    `});
+  if(interaction.commandName==="info"){
+    if(interaction._subcommand==="server"){
+      const servsr=interaction.guild;
+      console.log(server);
+      await interaction.reply({content:`サーバー名:${server.name}\n
+      サーバーID:${server.id}/n
+      メンバー数:${server.memberCount}\n
+      `});
+    }
   }
 }
 
