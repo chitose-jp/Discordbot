e242d8d923ab9b5f6e96d541b44f17dadfd0581d
ちとせあめ <chitoseame.jp@gmail.com>
2025-04-06 21:53:35 +0900

    Update index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,6 +4,13 @@ function handleCommand(interaction){
   if(interaction.commandName==="ping") {
     await interaction.reply({content:"Pong!",ephemeral:true});
   }
+  if(interaction.commandName==="info server") {
+    const servsr=interaction.guild;
+    console.log(server);
+    await interaction.reply({content:`サーバー名:${server.name}\n
+    メンバー数:${server.memberCount}\n
+    `});
+  }
 }
 
 module.exports=handleCommand;
